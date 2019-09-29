273936ca2646f7a5739eec8a1e9409f8337f9a99
andres Calfulef <rcalfulef@gmail.com>
2019-09-29 20:18:59 -0300

    falta hacer los containter

---
+++ app_cliente/client.py
@@ -0,0 +1,46 @@
+import socket
+ 
+def Main():
+    host = str(input("ingrese ip del servidor: "))
+    port = int(input("ingrese puerto del servidor: "))
+    
+    mySocket = socket.socket()
+    try:
+        mySocket.connect((host,port))
+    except:
+        print("no se ha podido realizar la conexion")
+        exit(1)
+    mySocket.send("--conexion--".encode())
+    data = recibir_input(mySocket,1024)
+    print(data)
+    print("Ingrese 'salir' para cerrar programa:")
+    message = input("Ingrese un mensaje: ")
+    
+    while message != 'salir':
+        mySocket.send(message.encode())
+        data = recibir_input(mySocket,1024)
+        if "--conexion--" in data:
+            print("conexion exitosa!")
+            message = input("Ingrese un mensaje: ")
+        else:
+            print ('Recibido desde el servidor: ' + data)
+            message = input("Ingrese un mensaje: ")
+    
+    mySocket.send(b"--quit--")
+
+def recibir_input(mySocket,max_buffer_size):
+    input_servidor = mySocket.recv(max_buffer_size)
+    decoded_input = input_servidor.decode()
+    resultado_guardar = guardar_en_archivo(decoded_input)
+    if resultado_guardar == True:
+        return decoded_input
+ 
+def guardar_en_archivo(input_str):
+    file = open("respuestas.txt","a")
+    file.write(input_str + "\n")
+    file.close()
+    
+    return True
+ 
+if __name__ == '__main__':
+    Main()
---
+++ app_headnode/headnode.py
@@ -0,0 +1,102 @@
+import socket
+from threading import Thread, Timer
+from random import choice
+cant_datanodes = 1
+
+def Main():
+    host = "0.0.0.0"  # Ip host
+    port = int(input("ingrese puerto a utilizar en el headnode: "))
+    mySocket = socket.socket()  # Instanciamos un socket
+    
+    sockets = sockets_datanodes(cant_datanodes) # Sockets para conectarse a los datanodes
+    multicast(sockets) # se encarga de realizar multicast crear un archivo
+    
+    try:
+        mySocket.bind((host,port)) # La funcion bind recibe una tupla, que contiene el host y el puerto 	
+    except:
+        print("no se ha podido crear el socket del servidor")
+        exit(1)
+	
+    mySocket.listen(3)  # le pasamos 1 para que escuche hasta que se cierre la conexion
+    print("servidor escuchando a clientes")
+    
+    while True:
+        conn, addr = mySocket.accept() # tenemos dos variables, una que tiene la conexion y la direccion del cliente
+        #print ("Conectado a : " + str(addr))		
+        
+        try:
+            Thread(target= cliente_thread, args=(conn,addr,sockets)).start() # le asignamos un hilo a un cliente
+        except:
+            print("El thread no pudo iniciar")
+    
+    mySocket.close()
+
+def cliente_thread(conn,addr,sockets,max_buffer_size=1024):
+    cliente_activo = True
+    
+    while cliente_activo:
+        input_cliente = recibir_input(conn,addr,max_buffer_size)
+        
+        if "--quit--" in input_cliente: # si el cliente envia salir
+            print("Un cliente solicita salir")
+            conn.close()
+            print("Conexion "+ str(addr) + " cerrada")
+            cliente_activo = False
+        if "--conexion--" in input_cliente:
+            print("nuevo cliente conectado: "+ str(addr))
+            conn.send("--conexion--".encode())
+        else: # si envia un mensaje
+            print("mensaje del cliente "+ str(addr))
+            aleatorio = choice(sockets)
+            aleatorio.send(input_cliente.encode())
+            respuesta = recibir_input_socket(aleatorio)
+            mensaje_a_cliente = "MENSAJE:\n    "+input_cliente+"\nEN "+str(aleatorio.getpeername())+'\n'
+            conn.send(mensaje_a_cliente.encode())
+        
+        
+def recibir_input(conn,addr,max_buffer_size=1024):
+    input_cliente = conn.recv(max_buffer_size)
+    decoded_input = input_cliente.decode()
+    resultado_guardar = guardar_en_archivo(decoded_input,addr)
+    if resultado_guardar == True:
+        return decoded_input
+
+def recibir_input_socket(mySocket,max_buffer_size=1024):
+    input_servidor = mySocket.recv(max_buffer_size)
+    decoded_input = input_servidor.decode()
+    #resultado_guardar = guardar_en_archivo(decoded_input)
+    #if resultado_guardar == True:
+    return decoded_input    
+
+def guardar_en_archivo(input_str,addr):
+    file  = open("log.txt","a")
+    file.write(input_str + " " +str(addr) + "\n")
+    file.close()
+    
+    return True
+
+def escribir(nombreArchivo,aEscribir):
+    var = open(nombreArchivo,"a")
+    var.write(aEscribir)
+    var.close()
+
+def sockets_datanodes(cant_datanodes):
+    lista = []
+    for i in range(cant_datanodes): # creamos los sockets para cada uno de los datanodes
+        sock = socket.socket()
+        dhost = str(input("ingrese ip del datanode "+ str(i) +" : "))
+        dport = int(input("ingrese puerto del datanode "+ str(i) +" : "))
+        sock.connect((dhost,dport))
+        lista.append(sock)
+    return lista
+	
+def multicast(sockets):
+    Timer(5.0,multicast,[sockets]).start()
+    for socket in sockets:
+        socket.send("--estado--".encode())
+        respuesta = recibir_input_socket(socket)
+        if "--vivo--" in respuesta:
+            escribir("hearbeat_server.txt",str(socket.getpeername())+"\n")
+                     
+if __name__ == '__main__':
+    Main()
